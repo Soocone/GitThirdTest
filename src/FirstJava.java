c5d17da5ab350659e27e3af1c1bf28f5d452d878
Soocone <wltn3185@naver.com>
2021-10-15 10:56:34 +0900

    학원 작업 v2.1

--- src/FirstJava.java
+++ src/FirstJava.java
@@ -4,5 +4,7 @@ public class FirstJava {
 	public static void main(String[] args) {
 		System.out.println("세번째 시도");
 		System.out.println("집에서 버전업 시도해보깅");
+		
+		System.out.println("학원에서 작업1");
 	}
 }
